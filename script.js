d9654d09420bca6c272e121a839e808fa81b3a65
Sebastian Carballo <sebastiancarballoes@gmail.com>
2023-11-05 19:09:47 -0300

    20th class     Adding [[14home-menuMobile]]     - media-queries for both menus: 	    - mobile-menu shown only on max-width: 640px 	    - desktop-menu shown only on min-width: 641px 		- one Javascript function for toggling inactive class on argument

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 
 const desktopMenu = document.querySelector('.desktop-menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+
 const navEmail = document.querySelector('.navbar-email');
+const burguerIcon = document.querySelector('.menu');
 
-navEmail.addEventListener('click', toggleDesktopMenu);
+navEmail.addEventListener('click', () => toggle(desktopMenu));
+burguerIcon.addEventListener('click',() => toggle(mobileMenu));
 
-function toggleDesktopMenu () {
-    console.log('Click');
-    desktopMenu.classList.toggle('inactive');
+function toggle(elemento){         
+    elemento.classList.toggle("inactive"); 
 }
-
-console.log('JS working');
