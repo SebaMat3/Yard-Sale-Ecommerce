87b6d15988aac1f9e4510f17e420e6b21401b701
Sebastian Carballo <sebastiancarballoes@gmail.com>
2023-11-06 12:06:21 -0300

    21th class. Dropdown  aside .shopping_cart  added to index. JS logic to avoid more than one menu open at once

--- script.js
+++ script.js
@@ -1,13 +1,28 @@
 
 const desktopMenu = document.querySelector('.desktop-menu');
 const mobileMenu = document.querySelector('.mobile-menu');
+const shoppingCart = document.querySelector('.product-details');
 
 const navEmail = document.querySelector('.navbar-email');
 const burguerIcon = document.querySelector('.menu');
+const cartIcon = document.querySelector('.navbar-shopping-cart');
 
-navEmail.addEventListener('click', () => toggle(desktopMenu));
-burguerIcon.addEventListener('click',() => toggle(mobileMenu));
+navEmail.addEventListener('click', toggleDesktopMenu);
+burguerIcon.addEventListener('click', toggleMobileMenu);
+cartIcon.addEventListener('click', toggleShoppingCart);
 
-function toggle(elemento){         
-    elemento.classList.toggle("inactive"); 
+function toggleDesktopMenu(){         
+    desktopMenu.classList.toggle('inactive');
+    mobileMenu.classList.add('inactive');
+    shoppingCart.classList.add('inactive');
+}
+function toggleMobileMenu(){         
+    desktopMenu.classList.add('inactive');
+    mobileMenu.classList.toggle('inactive');
+    shoppingCart.classList.add('inactive');
+}
+function toggleShoppingCart(){         
+    desktopMenu.classList.add('inactive');
+    mobileMenu.classList.add('inactive');
+    shoppingCart.classList.toggle('inactive');
 }
