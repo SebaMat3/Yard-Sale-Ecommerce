b2e0ec6f39c1f5b6990146ca6e652d70bffbaa33
Sebastian Carballo <sebastiancarballoes@gmail.com>
2023-11-07 07:37:33 -0300

    22th class. Main home1_signin products-view  merged. JS function with for loop prepared to render array of products on the html.

--- script.js
+++ script.js
@@ -7,6 +7,9 @@ const navEmail = document.querySelector('.navbar-email');
 const burguerIcon = document.querySelector('.menu');
 const cartIcon = document.querySelector('.navbar-shopping-cart');
 
+const cardsContainer = document.querySelector('.cards-container')
+
+
 navEmail.addEventListener('click', toggleDesktopMenu);
 burguerIcon.addEventListener('click', toggleMobileMenu);
 cartIcon.addEventListener('click', toggleShoppingCart);
@@ -26,3 +29,51 @@ function toggleShoppingCart(){
     mobileMenu.classList.add('inactive');
     shoppingCart.classList.toggle('inactive');
 }
+
+//Array containing products
+const productList = [];
+
+productList.push({
+    name: 'Bike',
+    price: '$220',
+    img: "https://images.pexels.com/photos/4542852/pexels-photo-4542852.jpeg?auto=compress&cs=tinysrgb&w=800"
+});
+productList.push({
+    name: 'Laptop',
+    price: '$1200',
+    img: "https://images.pexels.com/photos/4542852/pexels-photo-4542852.jpeg?auto=compress&cs=tinysrgb&w=800"
+});
+
+//function to introduce array elements into the HTML
+function renderProducts(arr) {
+    for (product of arr) {
+        const productCard = document.createElement('div');
+        productCard.classList.add('product-card');
+        cardsContainer.appendChild(productCard);
+            //Product-img
+            productImg = document.createElement('img');
+            productImg.setAttribute('src',product.img);
+            productImg.classList.add('product-img');
+
+            //Product-info container div
+            productInfo = document.createElement('div');
+            productInfo.classList.add('product-info');
+                productInfoDiv = document.createElement('div');
+                    productPrice = document.createElement('p');
+                    productName = document.createElement('p');
+                    productPrice.innerText = product.price;
+                    productName.innerText = product.name;
+                    productInfoDiv.append(productPrice,productName);
+                //AddToCart figure>img
+                addToCartFigure = document.createElement('figure');
+                    addToCartImg = document.createElement('img')
+                    addToCartImg.setAttribute('src','assets/icons/bt_add_to_cart.svg');
+                    addToCartImg.setAttribute('alt','add to cart icon');
+                    addToCartFigure.append(addToCartImg);
+            //Last tags nestings
+            productInfo.append(productInfoDiv,addToCartFigure);
+        productCard.append(productImg,productInfo);
+    }
+}
+
+renderProducts(productList);
